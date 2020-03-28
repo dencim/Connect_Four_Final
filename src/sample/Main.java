c8a922ede4431a6353315e13a3cf9e2bdd7a9295
dencim <43787288+dencim@users.noreply.github.com>
2020-03-27 21:33:42 -0400

    First commit

---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        GridPane board = new GridPane();
+        primaryStage.setTitle("Connect Four");
+        Scene scene = new Scene(board, 600, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
